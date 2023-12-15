6d2bec8409073a605bc2bab56060a06940dac9bb
Pritesh Jayprakash Verma <148872270+Pritesh-verma@users.noreply.github.com>
2023-12-16 00:28:43 +0530

    Add files via upload

--- Tester.java
+++ Tester.java
@@ -1,31 +1,19 @@
-// Tester.java
 public class Tester {
     public static void main(String[] args) {
-        // Test for CreditCardPayment
-        CreditCardPayment creditCardPayment = new CreditCardPayment();
-        creditCardPayment.balance = 5000.0;
-        System.out.println("Credit Card Payment Test 1:");
-        creditCardPayment.payBill(6000.0);
+        // Create an object of class InfyTV
+        InfyTV infyTV = new InfyTV();
 
-        CreditCardPayment creditCardPayment2 = new CreditCardPayment();
-        creditCardPayment2.balance = 3000.0;
-        System.out.println("\nCredit Card Payment Test 2:");
-        creditCardPayment2.payBill(2000.0);
+        // Try to access private members directly (This will result in a compilation error)
+        // infyTV.correspondent = "Kimberely";  // Error: correspondent has private access
+        // infyTV.newsReporter = "Hudson";      // Error: newsReporter has private access
+        // infyTV.photographer = "Joshua";      // Error: photographer has private access
 
-        // Test for ShoppingPayment
-        ShoppingPayment shoppingPayment = new ShoppingPayment();
-        shoppingPayment.balance = 1000.0;
-        System.out.println("\nShopping Payment Test 1:");
-        shoppingPayment.payBill(1000.0);
+        // Access private members using setters
+        infyTV.setCorrespondent("Kimberely");
+        infyTV.setNewsReporter("Hudson");
+        infyTV.setPhotographer("Joshua");
 
-        ShoppingPayment shoppingPayment2 = new ShoppingPayment();
-        shoppingPayment2.balance = 1500.0;
-        System.out.println("\nShopping Payment Test 2:");
-        shoppingPayment2.payBill(1200.0);
-
-        ShoppingPayment shoppingPayment3 = new ShoppingPayment();
-        shoppingPayment3.balance = 2000.0;
-        System.out.println("\nShopping Payment Test 3:");
-        shoppingPayment3.payBill(2500.0);
+        // Show the documentary to the public
+        infyTV.documentaryFilm();
     }
 }
